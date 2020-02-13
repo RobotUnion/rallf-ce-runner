d140a604d2f8bd7446e98da518440c0887868c03
nombrekeff <manoloedge96@gmail.com>
2020-02-13 13:14:51 +0100

    fixed example await missing

--- test-task/basic-example/src/main.js
+++ test-task/basic-example/src/main.js
@@ -3,7 +3,7 @@ const rallf = require('rallf-js-sdk');
 class BasicExample extends rallf.Task {
   async warmup() {
     this.logger.debug('Warming up ' + this.name);
-    this.firefox = this.devices.get('firefox');
+    this.firefox = await this.devices.get('firefox');
     await this.firefox.get('http://0.0.0.0:9001/#/');
     await this.robot.delegateLocal('test-task', 'like', {
       post: 'some post'
